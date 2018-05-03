c55380b28a1685925ea8351fe51def28d9bcc59b
Suchismitha <svedala@github.com>
2018-05-02 20:45:08 -0500

    report indexing

--- FINAL/Linear_Models.R
+++ FINAL/Linear_Models.R
@@ -78,14 +78,14 @@ ggsave(filename="Plot/SuturingVsSex.pdf", plot=p1)
 session=rep(c("Session1","Session2","Session3","Session4","Session5"),15)
 summary(lm(Sutures~Sex*session))
 
-p2=ggplot(sdata,aes(session, Sutures)) + theme(plot.title = element_text(hjust = 0.5)) + 
+p2=ggplot(sdata,aes(session, Sutures,fill=session)) + theme(plot.title = element_text(hjust = 0.5)) + 
   #geom_jitter(alpha=0.2) +geom_smooth(method=lm)+xlim(1,5)+
   geom_bar(stat="identity",fill="#56B4E9")+
   labs(x = 'Sessions', y = 'Number of Sutures')+ggtitle("Performance WRT number of sutures made summing  values of all subjects")
 ggsave(filename="Plot/PerformanceWRTSutures_bar.pdf", plot=p2)
 
 
-p3=ggplot(sdata,aes(session, Sutures)) +
+p3=ggplot(sdata,aes(session, Sutures,fill=session)) +
   geom_jitter(alpha=0.2) +geom_smooth(method=lm)+xlim(1,5)+ theme(plot.title = element_text(hjust = 0.5)) + 
   #geom_bar(stat="identity",fill="#56B4E9")+
   labs(x = 'Sessions', y = 'Number of Sutures')+ggtitle("Performance WRT number of sutures made")
@@ -105,51 +105,51 @@ summary(lm(formula = Scores~log(Normalised_PP)+Age+Sex+Task+Scorer+Session,data=
 rand_data=na.omit(data)
 anova(lme(Scores~Normalised_PP+Age+Year+Sex+Task+Session,random=~1|Subject,data=rand_data))
 
-p4 = ggplot(data, aes(x=Scorer, y=Scores))  + theme(plot.title = element_text(hjust = 0.5)) +  
+p4 = ggplot(data, aes(x=Scorer, y=Scores,fill=Scorer))  + theme(plot.title = element_text(hjust = 0.5)) +  
   geom_boxplot()+ggtitle("Analysis of Scores based on Scorer") +labs(x="Scorer Number",y="Number of Scores")
 ggsave(filename="Plot/ScorerVsScore.pdf", plot=p4)
 
 
-p5 = ggplot(data, aes(x=Sex, y=Scores))  + theme(plot.title = element_text(hjust = 0.5)) +  
+p5 = ggplot(data, aes(x=Sex, y=Scores,fill=Sex))  + theme(plot.title = element_text(hjust = 0.5)) +  
   geom_boxplot()+ggtitle("Analysis of Scores based on Gender") +labs(x="Gender",y="Number of Scores")
 ggsave(filename="Plot/GenderVsScore.pdf", plot=p5)
 
 
-p6 = ggplot(data, aes(x=Session, y=Scores))  + theme(plot.title = element_text(hjust = 0.5)) +  
+p6 = ggplot(data, aes(x=Session, y=Scores,fill=Session))  + theme(plot.title = element_text(hjust = 0.5)) +  
   geom_boxplot()+ggtitle("Analysis of Scores based on Scorer") +labs(x="Session Number",y="Number of Scores")
 ggsave(filename="Plot/SessionVsScore.pdf", plot=p6)
 
 
-p7 = ggplot(data, aes(x=Task, y=Scores))  + theme(plot.title = element_text(hjust = 0.5)) +  
+p7 = ggplot(data, aes(x=Task, y=Scores,fill=Task))  + theme(plot.title = element_text(hjust = 0.5)) +  
   geom_boxplot()+ggtitle("Analysis of Scores based on Task") +labs(x="Task",y="Number of Scores")
 ggsave(filename="Plot/TaskVsScore.pdf", plot=p7)
 
 #Summary_Based on Time with all other attributes
 summary(lm(formula = Time~log(Normalised_PP)+Age+Sex+Task+Session,data=data))
 
-p8 = ggplot(data, aes(x=Scorer, y=Time))  + theme(plot.title = element_text(hjust = 0.5)) +  
+p8 = ggplot(data, aes(x=Scorer, y=Time,fill=Scorer))  + theme(plot.title = element_text(hjust = 0.5)) +  
   geom_boxplot()+ggtitle("Analysis of Time based on Scorer") +labs(x="Scorer Number",y="Time in seconds")
 ggsave(filename="Plot/ScorerVsTime.pdf", plot=p8)
 
 
-p9 = ggplot(data, aes(x=Sex, y=Time)) + theme(plot.title = element_text(hjust = 0.5)) +  
+p9 = ggplot(data, aes(x=Sex, y=Time,fill=Sex)) + theme(plot.title = element_text(hjust = 0.5)) +  
   geom_boxplot()+ggtitle("Analysis of Time based on Gender") +labs(x="Gender",y="Time in seconds")
 ggsave(filename="Plot/GenderVsTime.pdf", plot=p9)
 
 
-p10 = ggplot(data, aes(x=Session, y=Time))  + theme(plot.title = element_text(hjust = 0.5)) +  
+p10 = ggplot(data, aes(x=Session, y=Time,fill=Session))  + theme(plot.title = element_text(hjust = 0.5)) +  
   geom_boxplot()+ggtitle("Analysis of Time based on Scorer") +labs(x="Session Number",y="Time in seconds")
 ggsave(filename="Plot/SessionVsTime.pdf", plot=p10)
 
 
-p11 = ggplot(data, aes(x=Task,y=Time))  + theme(plot.title = element_text(hjust = 0.5)) +  
+p11 = ggplot(data, aes(x=Task,y=Time,fill=Task))  + theme(plot.title = element_text(hjust = 0.5)) +  
   geom_boxplot()+ggtitle("Analysis of Time based on Task") +labs(x="Task",y="Time in seconds")
 ggsave(filename="Plot/TaskVsTime.pdf", plot=p11)
 
 
 #Analysis based on Scorer
 Cutting=read.csv("Data/Cutting_Data.csv")
-p12=ggplot(Cutting, aes(x=Scorer, y=Scores)) + theme(plot.title = element_text(hjust = 0.5)) +  
+p12=ggplot(Cutting, aes(x=Scorer, y=Scores,fill=Scorer)) + theme(plot.title = element_text(hjust = 0.5)) +  
   geom_boxplot()+ggtitle("Analysis of Scores based on Scorer") +labs(x="Scorer Number",y="Number of Scorer")
 ggsave(filename="Plot/Cutting_ScorerVsScore.pdf", plot=p12)
 #Wilcox Test
@@ -158,7 +158,7 @@ CScorer2=as.numeric(Cutting[76:150,7])
 wilcox.test(CScorer2,CScorer1, paired=TRUE, alternative ="two.sided")
 
 Suturing=read.csv("Data/Suturing_Data.csv")
-p13=ggplot(Suturing, aes(x=Scorer, y=Scores)) + 
+p13=ggplot(Suturing, aes(x=Scorer, y=Scores,fill=Scorer)) + 
   geom_boxplot()+ggtitle("Analysis of Scores based on Scorer") +labs(x="Scorer Number",y="Number of Scorer")
 ggsave(filename="Plot/Suturing_ScorerVsScore.pdf", plot=p13)
 SScorer1=as.numeric(Suturing[1:75,7])
